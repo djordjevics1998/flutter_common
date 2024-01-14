5d1ce1f8768a22cdb7022a6c2673a9606590a178
kamileon <s.djordjevic1998@gmail.com>
2024-01-14 18:34:55 +0100

    go_router ext

---
+++ lib/utils/ext_go_router.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+void goOrReplace(BuildContext context, String route, {Map<String, String> pathParameters = const <String, String>{},
+  Map<String, dynamic> queryParameters = const <String, dynamic>{},}) {
+  if(GoRouter.of(context).location() != context.namedLocation(route, pathParameters: pathParameters, queryParameters: queryParameters)) {
+    context.go(context.namedLocation(route, pathParameters: pathParameters, queryParameters: queryParameters));
+  } else {
+    context.push(GoRouter.of(context).location());//context.namedLocation(route, pathParameters: pathParameters, queryParameters: queryParameters));
+  }
+}
+
+extension GoRouterExtension on GoRouter {
+  String location() {
+    final RouteMatch lastMatch = routerDelegate.currentConfiguration.last;
+    final RouteMatchList matchList = lastMatch is ImperativeRouteMatch ? lastMatch.matches : routerDelegate.currentConfiguration;
+    final String location = matchList.uri.toString();
+    return location;
+  }
+}
