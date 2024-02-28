9aea25cb3868b26f10edd8f6e8f35ea8f8896bec
kamileon <s.djordjevic1998@gmail.com>
2024-02-28 17:44:02 +0100

    switch to unihtml

--- lib/utils/share_text.dart
+++ lib/utils/share_text.dart
@@ -2,8 +2,8 @@ import 'dart:async';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'dart:html' as html;
-import 'dart:js' as js;
+import 'package:universal_html/html.dart' as html;
+import 'package:universal_html/js.dart' as js;
 
 import 'package:flutter/services.dart';
 import 'package:share_plus/share_plus.dart';
