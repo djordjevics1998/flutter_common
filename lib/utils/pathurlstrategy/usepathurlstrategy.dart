5205f29ba4de9c3df98114aa92de2d88f6b0659a
kamileon <s.djordjevic1998@gmail.com>
2024-03-29 01:58:55 +0100

    usepathurlstrategy cross-platform

---
+++ lib/utils/pathurlstrategy/usepathurlstrategy.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_web_plugins/flutter_web_plugins.dart'
+if (dart.library.html) 'package:flutter_web_plugins/flutter_web_plugins.dart'
+if (dart.library.io) './pathurlstrategy_stub.dart'
+as web;
+
+void cpUsePathUrlStrategy() => web.usePathUrlStrategy();
