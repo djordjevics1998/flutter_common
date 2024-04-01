5c7b98c74724417f4cabe8bd1c38549595415b31
kamileon <s.djordjevic1998@gmail.com>
2024-04-01 22:31:41 +0200

    dsadas

--- lib/utils/ext_go_router.dart
+++ lib/utils/ext_go_router.dart
@@ -1,16 +1,18 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:universal_html/html.dart';
 
 void goOrReplace(BuildContext context, String route, {Map<String, String> pathParameters = const <String, String>{},
   Map<String, dynamic> queryParameters = const <String, dynamic>{},}) {
   final loc = GoRouter.of(context).location();
-  debugPrint(loc);
+  //debugPrint(loc);
 
   if(/*GoRouterState.of(context).uri.toString()*/loc != context.namedLocation(route, pathParameters: pathParameters, queryParameters: queryParameters)) {
     context.go(context.namedLocation(route, pathParameters: pathParameters, queryParameters: queryParameters));
   } else {
     //context.pop();
-    context.pushReplacement(context.namedLocation(route, pathParameters: pathParameters, queryParameters: queryParameters));
+    kIsWeb ? window.location.reload() : context.pushReplacement(loc);
     //context.push(context.namedLocation(route, pathParameters: pathParameters, queryParameters: queryParameters));
     //context.replace(/*GoRouterState.of(context).uri.toString()*/ loc);//GoRouter.of(context).location());//context.namedLocation(route, pathParameters: pathParameters, queryParameters: queryParameters));
   }
