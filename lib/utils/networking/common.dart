60862b8294b38aba82687ca0707c316175491864
kamileon <s.djordjevic1998@gmail.com>
2024-04-02 19:25:24 +0200

    http status codes

---
+++ lib/utils/networking/common.dart
@@ -0,0 +1,6 @@
+const httpStatusCodeOk = 200,
+  httpStatusCodeBadRequest = 400,
+  httpStatusCodeNotFound = 404,
+  httpStatusCodeTimeout = 408,
+  httpStatusCodeServerInternalError = 500,
+  httpStatusCodeServerTimeout = 504;
