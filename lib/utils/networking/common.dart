a187da3e9f377f3276e77b4439a0ff7687d10d95
kamileon <s.djordjevic1998@gmail.com>
2024-04-04 12:02:15 +0200

    errormessage typedef

--- lib/utils/networking/common.dart
+++ lib/utils/networking/common.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/material.dart';
+
 const httpStatusCodeOk = 200,
   httpStatusCodeBadRequest = 400,
   httpStatusCodeNotFound = 404,
   httpStatusCodeTimeout = 408,
   httpStatusCodeServerInternalError = 500,
   httpStatusCodeServerTimeout = 504;
+
+typedef ErrorMessage = String Function(BuildContext);
