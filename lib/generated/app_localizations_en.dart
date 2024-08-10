2e724f90a2289d63cdbd84d387baf76613df27d3
Stefan Djordjevic <s.djordjevic1998@gmail.com>
2024-08-10 17:11:18 +0200

    pub upgrade

--- lib/generated/app_localizations_en.dart
+++ lib/generated/app_localizations_en.dart
@@ -1,7 +1,5 @@
 import 'app_localizations.dart';
 
-// ignore_for_file: type=lint
-
 /// The translations for English (`en`).
 class FlutterCommonLocalizationsEn extends FlutterCommonLocalizations {
   FlutterCommonLocalizationsEn([String locale = 'en']) : super(locale);
