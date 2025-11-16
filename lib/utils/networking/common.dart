37d605d5e808dc560797a9f805c650eeaea93c78
kamileon <s.djordjevic1998@gmail.com>
2025-11-16 19:56:02 +0100

    http codes

--- lib/utils/networking/common.dart
+++ lib/utils/networking/common.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 const httpStatusCodeOk = 200,
-  httpStatusCodeBadRequest = 400,
-  httpStatusCodeNotFound = 404,
-  httpStatusCodeTimeout = 408,
-  httpStatusCodeServerInternalError = 500,
-  httpStatusCodeServerTimeout = 504;
+    httpStatusCodeBadRequest = 400,
+    httpStatusCodeUnauthorizedRequest = 401,
+    httpStatusCodeNotFound = 404,
+    httpStatusCodeTimeout = 408,
+    httpStatusCodeServerInternalError = 500,
+    httpStatusCodeServerTimeout = 504;
 
 typedef ErrorMessage = String Function(BuildContext);
